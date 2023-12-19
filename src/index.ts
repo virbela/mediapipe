301078b64ac1e122a2466af3913fd2aca0c28cee
Jason Shen <jason_2000_nz@hotmail.com>
2023-12-19 23:52:21 +1300

    change the model

--- src/index.ts
+++ src/index.ts
@@ -47,11 +47,12 @@ export const HandGuesture = async () => {
   // Create task for image file processing:
 const vision = await FilesetResolver.forVisionTasks(
   // path/to/wasm/root
-  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm "
+  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
 );
 const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
   baseOptions: {
-    modelAssetPath: "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task"
+    modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+    delegate: "GPU"
   },
   numHands: 2
 });
